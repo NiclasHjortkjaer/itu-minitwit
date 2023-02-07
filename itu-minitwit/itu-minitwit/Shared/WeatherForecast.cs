e662a1e322d9b200977385ca819d7d2f3429d68c
carcasonne <villumsonne@tutanota.com>
2023-02-07 17:09:55 +0100

    Setup .NET project. Blazor client, ASP.NET server

---
+++ itu-minitwit/itu-minitwit/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace itu_minitwit.Shared;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
