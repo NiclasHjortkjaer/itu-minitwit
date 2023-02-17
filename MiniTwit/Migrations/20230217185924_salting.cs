45aba36af80eaffd81ae272c0a80560391913c1d
luhj <luhj@itu.dk>
2023-02-17 20:12:59 +0100

    migrations

---
+++ MiniTwit/Migrations/20230217185924_salting.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniTwit.Migrations
+{
+    /// <inheritdoc />
+    public partial class salting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+    }
+}
