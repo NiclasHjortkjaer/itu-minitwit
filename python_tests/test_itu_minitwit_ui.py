1bc0ff7c8737b5906a8331815cd4f8e1d1389cd7
luhj <luhj@itu.dk>
2023-03-18 11:29:48 +0100

    ci: add more ui tests

--- python_tests/test_itu_minitwit_ui.py
+++ python_tests/test_itu_minitwit_ui.py
@@ -1,28 +1,3 @@
-"""
-To run this test with a visible browser, the following dependencies have to be setup:
-
-  * `pip install selenium`
-  * `pip install pymongo`
-  * `pip install pytest`
-  * `wget https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz`
-  * `tar xzvf geckodriver-v0.32.0-linux64.tar.gz`
-  * After extraction, the downloaded artifact can be removed: `rm geckodriver-v0.32.0-linux64.tar.gz`
-
-The application that it tests is the version of _ITU-MiniTwit_ that you got to know during the exercises on Docker:
-https://github.com/itu-devops/flask-minitwit-mongodb/tree/Containerize (*OBS*: branch Containerize)
-
-```bash
-$ git clone https://github.com/HelgeCPH/flask-minitwit-mongodb.git
-$ cd flask-minitwit-mongodb
-$ git switch Containerize
-```
-
-After editing the `docker-compose.yml` file file where you replace `youruser` with your respective username, the
-application can be started with `docker-compose up`.
-
-Now, the test itself can be executed via: `pytest test_itu_minitwit_ui.py`.
-"""
-
 import psycopg2
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -33,7 +8,7 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
 
-GUI_URL = "http://localhost:8765/register"
+GUI_URL = "http://localhost:8765"
 DB_URL = "postgres://localhost:2345"
 
 try:
@@ -43,8 +18,43 @@ except:
 
 cur = conn.cursor()
 
+def _get_user_by_name(name):
+    cur.execute(f"""SELECT * FROM "Users" WHERE "Username" = '{name}';""")
+    user = cur.fetchall()
+    return None if user == [] else user[0]
+
+def _delete_user_by_name(name):
+    cur.execute(f"""DELETE FROM "Users" WHERE "Username" = '{name}';""")
+    conn.commit()
+
+def _get_user_tweets_by_name(name):
+    user = _get_user_by_name(name)
+    cur.execute(f"""SELECT "Text" FROM "Messages" WHERE "AuthorId" = {user[0]};""")
+    tweets = cur.fetchall()
+    return map(lambda m : m[0], tweets)
+
+def _get_follows_by_name(name):
+    user = _get_user_by_name(name)
+    cur.execute(
+        f"""
+        SELECT u."Username"
+        FROM "UserUser" f
+        JOIN "Users" u ON u."Id" = f."FollowsId"
+        WHERE f."FollowersId" = {user[0]};
+        """)
+    follows = cur.fetchall()
+    return map(lambda f : f[0], follows)
+
+
+# seed with "You" user
+if (_get_user_by_name('You') == None):
+    cur.execute("""INSERT INTO "Users" VALUES (-1, 'You', 'You@mail.com', 'PASSWORD', 'HASH')""")
+    cur.execute("""INSERT INTO "Messages" VALUES (-1, -1, 'Read this', '2023-03-01', 0)""")
+    cur.execute("""INSERT INTO "Messages" VALUES (-2, -1, 'I am You', '2023-03-02', 0)""")
+    conn.commit()
+
 def _register_user_via_gui(driver, data):
-    driver.get(GUI_URL)
+    driver.get(f"{GUI_URL}/register")
 
     wait = WebDriverWait(driver, 5)
     buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "actions")))
@@ -59,16 +69,58 @@ def _register_user_via_gui(driver, data):
 
     return flashes
 
+def _login_user_via_gui(driver, data):
+    driver.get(f"{GUI_URL}/login")
 
-def _get_user_by_name(name):
-    cur.execute(f"""SELECT "Username" FROM "Users" WHERE "Username" = '{name}';""")
-    user = cur.fetchall()
-    return None if user == [] else user[0]
+    wait = WebDriverWait(driver, 5)
+    buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "actions")))
+    input_fields = driver.find_elements(By.TAG_NAME, "input")
 
-def _delete_user_by_name(name):
-    cur.execute(f"""DELETE FROM "Users" WHERE "Username" = '{name}';""")
-    conn.commit()
+    for idx, str_content in enumerate(data):
+        input_fields[idx].send_keys(str_content)
+    input_fields[2].send_keys(Keys.RETURN)
 
+    wait = WebDriverWait(driver, 5)
+    flashes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "flashes")))
+
+    return flashes
+
+def _logout_user_via_gui(driver):
+    wait = WebDriverWait(driver, 5)
+    driver.find_element(By.PARTIAL_LINK_TEXT, "sign out").click()
+
+    wait = WebDriverWait(driver, 5)
+    flashes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "flashes")))
+
+    return flashes
+
+def _tweet_via_gui(driver, data):
+    driver.get(f"{GUI_URL}/")
+
+    wait = WebDriverWait(driver, 5)
+    buttons = wait.until(EC.presence_of_all_elements_located((By.ID, "Text")))
+    input_fields = driver.find_elements(By.TAG_NAME, "input")
+
+    for idx, str_content in enumerate(data):
+        input_fields[idx].send_keys(str_content)
+    input_fields[1].send_keys(Keys.RETURN)
+
+    wait = WebDriverWait(driver, 5)
+    flashes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "flashes")))
+
+    return flashes
+
+def _follow_via_gui(driver, data):
+    driver.get(f"{GUI_URL}/{data[0]}")
+
+    wait = WebDriverWait(driver, 5)
+    buttons = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "link-button")))
+    driver.find_element(By.TAG_NAME, "button").click()
+
+    wait = WebDriverWait(driver, 5)
+    flashes = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "flashes")))
+
+    return flashes
 
 def test_register_user_via_gui():
     """
@@ -96,6 +148,7 @@ def test_register_user_via_gui_and_check_db_entry():
     firefox_options = Options()
     firefox_options.add_argument("--headless")
     # firefox_options = None
+
     with webdriver.Firefox(service=Service("./geckodriver"), options=firefox_options) as driver:
 
         assert _get_user_by_name('Me') == None
@@ -104,8 +157,92 @@ def test_register_user_via_gui_and_check_db_entry():
         expected_msg = "You were successfully registered and can login now."
         assert generated_msg == expected_msg
 
-        
-        assert _get_user_by_name("Me")[0] == "Me"
+        assert _get_user_by_name("Me")[1] == "Me"
 
         # cleanup, make test case idempotent
+        _delete_user_by_name('Me')
+
+def test_register_then_login():
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    # firefox_options = None
+    with webdriver.Firefox(service=Service("./geckodriver"), options=firefox_options) as driver:
+        assert _get_user_by_name('Me') == None
+
+        _register_user_via_gui(driver, ["Me", "me@some.where", "secure123", "secure123"])
+        generated_msg = _login_user_via_gui(driver, ["Me", "secure123"])[0].text
+        expected_msg = "You were logged in."
+
+        assert generated_msg == expected_msg
+
+        _delete_user_by_name('Me')
+
+def test_register_login_then_logout():
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    # firefox_options = None
+    with webdriver.Firefox(service=Service("./geckodriver"), options=firefox_options) as driver:
+        assert _get_user_by_name('Me') == None
+
+        _register_user_via_gui(driver, ["Me", "me@some.where", "secure123", "secure123"])
+        _login_user_via_gui(driver, ["Me", "secure123"])
+        generated_msg = _logout_user_via_gui(driver)[0].text
+        expected_msg = "You were logged out."
+
+        assert generated_msg == expected_msg
+
+        _delete_user_by_name('Me')
+
+def test_register_login_then_tweet():
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    # firefox_options = None
+    with webdriver.Firefox(service=Service("./geckodriver"), options=firefox_options) as driver:
+        assert _get_user_by_name('Me') == None
+
+        _register_user_via_gui(driver, ["Me", "me@some.where", "secure123", "secure123"])
+        _login_user_via_gui(driver, ["Me", "secure123"])
+        generated_msg = _tweet_via_gui(driver, ["Hello"])[0].text
+        expected_msg = "Your message was recorded."
+        
+        assert generated_msg == expected_msg
+        
+        generated_msg = _tweet_via_gui(driver, ["My password is very secure"])[0].text
+        generated_msg = _tweet_via_gui(driver, ["123"])[0].text
+
+        msgs = _get_user_tweets_by_name('Me')
+
+        assert "Hello" in msgs
+        assert "My password is very secure" in msgs
+        assert "123" in msgs
+
+        _delete_user_by_name('Me')
+
+def test_register_login_then_follow():
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    # firefox_options = None
+    with webdriver.Firefox(service=Service("./geckodriver"), options=firefox_options) as driver:
+        assert _get_user_by_name('Me') == None
+
+        _register_user_via_gui(driver, ["Me", "me@some.where", "secure123", "secure123"])
+        _login_user_via_gui(driver, ["Me", "secure123"])
+        generated_msg = _follow_via_gui(driver, ["You"])[0].text
+        expected_msg = "You are now following You."
+        
+        assert generated_msg == expected_msg
+
+        fllws = _get_follows_by_name('Me')
+
+        assert "You" in fllws
+
+        driver.get(f"{GUI_URL}/")
+        wait = WebDriverWait(driver, 5)
+        msgs = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "li")))
+        elements = driver.find_elements(By.TAG_NAME, "li")
+        msgs = list(map(lambda m : m.text, elements))
+
+        assert len([m for m in msgs if "Read this" in m]) == 1
+        assert len([m for m in msgs if "I am You" in m]) == 1
+
         _delete_user_by_name('Me')
