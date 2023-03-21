b572289d1d0daec77248a3d6d9ae942bcb845e56
NiclasHjortkjaer <hjortkjaer.niclas@gmail.com>
2023-03-21 18:43:44 +0100

    ci: add logging to userrepository

--- MiniTwit/Repositories/UserRepository.cs
+++ MiniTwit/Repositories/UserRepository.cs
@@ -23,11 +23,12 @@ public class UserRepository : IUserRepository
     
     public async Task<User> Register(string username, string email, string password, string passwordRepeat)
     {
-        if (password != passwordRepeat) throw new ArgumentException("The two passwords do not match");
-        if (username.Trim() != username) throw new ArgumentException("Username cannot begin or end with space");
-        if (!Regex.IsMatch(username, @"^[a-zA-Z0-9 _]+$") || username.ToLower() is "public" or "login" or "register" or "logout" or "twithub" or "mytimeline") throw new ArgumentException("Forbidden username.");
-        if (await _miniTwitContext.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower())) throw new ArgumentException("Username is already taken.");
-        if (await _miniTwitContext.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower())) throw new ArgumentException("Email is already taken.");
+        if (password != passwordRepeat) ThrowArgumentException(username, "The two passwords do not match.");
+        if (username.Trim() != username) ThrowArgumentException(username, "Username cannot begin or end with space.");
+        if (!Regex.IsMatch(username, @"^[a-zA-Z0-9 _]+$") ||
+            username.ToLower() is "public" or "login" or "register" or "logout" or "twithub" or "mytimeline") ThrowArgumentException(username, "Forbidden username.");
+        if (await _miniTwitContext.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower())) ThrowArgumentException(username, "Username is already taken.");
+        if (await _miniTwitContext.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower())) ThrowArgumentException(username, "Email is already taken.");
 
         var salt = Guid.NewGuid().ToString("N").Replace("\u0000", "");
         var user = new User() { 
@@ -41,14 +42,15 @@ public class UserRepository : IUserRepository
         _miniTwitContext.Users.Add(user);
         await _miniTwitContext.SaveChangesAsync();
 
+        _logger.LogInformation("Registered user: {username} at {time}", username, DateTime.UtcNow.ToLongTimeString());
         return user;
     }
 
     public async Task<User> Login(string username, string password)
     {
         var user = await _miniTwitContext.Users.FirstOrDefaultAsync(u => u.Username.ToLower() == username.ToLower());
-        if (user == null) throw new ArgumentException("Invalid username.");
-        if (HashPassword(password, user.Salt) != user.PwHash) throw new ArgumentException("Invalid password.");
+        if (user == null) ThrowArgumentException(username, "Invalid username.");
+        if (HashPassword(password, user.Salt) != user.PwHash) ThrowArgumentException(username, "Invalid password.");
 
         var claims = new List<Claim>
         {
@@ -70,10 +72,12 @@ public class UserRepository : IUserRepository
     {
         await _httpContextAccessor.HttpContext.SignOutAsync(
             CookieAuthenticationDefaults.AuthenticationScheme);
+        _logger.LogInformation("Logged out at {time}", DateTime.UtcNow.ToLongTimeString());
     }
 
     public async Task<User?> GetCurrent()
     {
+        _logger.LogInformation("GetCurrent user at {time}", DateTime.UtcNow.ToLongTimeString());
         var claims = _httpContextAccessor.HttpContext!.User.Identity as ClaimsIdentity;
         var hasId = int.TryParse(claims!.FindFirst("id")?.Value, out int id);
         if (hasId)
@@ -87,7 +91,7 @@ public class UserRepository : IUserRepository
             .Include(u => u.Followers)
             .FirstOrDefaultAsync(u => u.Username.ToLower() == username.ToLower());
         var currentUser = await GetCurrent();
-        if (user == null || currentUser == null) throw new ArgumentException();
+        if (user == null || currentUser == null) ThrowArgumentException(username, "username cannot be null");
 
         return user.Followers.Contains(currentUser);
     }
@@ -98,17 +102,19 @@ public class UserRepository : IUserRepository
             .Include(u => u.Followers)
             .FirstOrDefaultAsync(u => u.Username.ToLower() == username.ToLower());
         var currentUser = await GetCurrent();
-        if (user == null || currentUser == null) throw new ArgumentException();
+        if (user == null || currentUser == null) ThrowArgumentException(username, "username cannot be null");
 
         if (user.Followers.Contains(currentUser))
         {
             user.Followers.Remove(currentUser);
             _httpContextAccessor.HttpContext.Session.SetString("flashes", $"You are no longer following {username}.");
+            _logger.LogInformation("{username} started following {currentUser} at {time}", username, currentUser.Username, DateTime.UtcNow.ToLongTimeString());
         }
         else
         {
             user.Followers.Add(currentUser);
             _httpContextAccessor.HttpContext.Session.SetString("flashes", $"You are now following {username}.");
+            _logger.LogInformation("{username} unfollowed {currentUser} at {time}", username, currentUser.Username, DateTime.UtcNow.ToLongTimeString());
         }
 
         await _miniTwitContext.SaveChangesAsync();
@@ -116,11 +122,13 @@ public class UserRepository : IUserRepository
 
     public async Task<User?> Exists(string username)
     {
+        _logger.LogInformation("Extists {username} called at {time}", username, DateTime.UtcNow.ToLongTimeString());
         return await _miniTwitContext.Users.FirstOrDefaultAsync(u => u.Username.ToLower() == username.ToLower());
     }
 
     public async Task<IEnumerable<User?>?> GetFollows(string username)
     {
+        _logger.LogInformation("GetFollows {username} called at {time}", username, DateTime.UtcNow.ToLongTimeString());
         var user = await _miniTwitContext.Users
             .Include(u => u.Follows)
             .FirstOrDefaultAsync(u => string.Equals(u.Username.ToLower(), username.ToLower()));
@@ -132,5 +140,11 @@ public class UserRepository : IUserRepository
     {
         var hashed = SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(password + salt));
         return System.Text.Encoding.Default.GetString(hashed).Replace("\u0000", "");
+    }
+    
+    private void ThrowArgumentException(string username, string errorMsg)
+    {
+        _logger.LogError("ArgumentException - Registering user: {username} failed at {time}. Error message: {error}", username, DateTime.UtcNow.ToLongTimeString(), errorMsg);
+        throw new ArgumentException(errorMsg);
     }
 }
